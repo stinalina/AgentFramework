11b5fcd533a93d8939ec207fb0e0385bf466d829
stinalina <38920418+stinalina@users.noreply.github.com>
2026-02-14 07:35:35 +0100

    feat: first agent quick start

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using OpenAI.Chat;
+
+AIAgent agent = new AzureOpenAIClient(
+  new Uri("https://oai-coco.openai.azure.com/"),
+  new AzureCliCredential())
+    .GetChatClient("TripAdvisor_Agent")
+    .AsAIAgent(instructions: "You are good at telling jokes.");
+
+Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
+
+// Stream the response
+//await foreach (var update in agent.RunStreamingAsync("Tell me a one-sentence fun fact."))
+//{
+//  Console.Write(update);
+//}
